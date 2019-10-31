c9143847d7e84af388cc939c2762db8b15bc9c62
Morgan <michaeladesola1410@gmail.com>
2019-10-31 03:09:48 +0100

    completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -34,7 +34,11 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+       Uri telephoneNumber =   Uri.parse("tel:0123456789");
+        phoneIntent.setData(telephoneNumber);
+        startActivity(phoneIntent);
+    }
 }
